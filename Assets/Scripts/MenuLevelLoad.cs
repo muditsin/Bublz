9d199a2a693ee541ede61c1009b3eab53438947b
muditsin <muditsin@outlook.com>
2014-12-30 00:02:09 +0530

    Change

--- Assets/Scripts/MenuLevelLoad.cs
+++ Assets/Scripts/MenuLevelLoad.cs
@@ -1,40 +1,40 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
-public class MenuLevelLoad : MonoBehaviour
-{
+public class MenuLevelLoad : MonoBehaviour {
+
 	public Button levelSelect;
+	public Animator MenuGUI;
 
-	void Start()
-	{
-		if(!PlayerPrefs.HasKey("Played"))
-			levelSelect.enabled = false;
+	// Use this for initialization
+	void Start () {
+		if (!PlayerPrefs.HasKey ("Played")) {
+			levelSelect.enabled=false;
+		}
+		PlayerPrefs.SetInt ("ToRetry", 0);
 	}
 	
 	// Update is called once per frame
-	void Update()
-	{
+	void Update () {
 	
 	}
+	public void LevelStart(){
 
-	public void LevelStart()
-	{
-		PlayerPrefs.SetInt("CurrentLevel", 0);
+		PlayerPrefs.SetInt ("CurrentLevel",0);
 		Application.LoadLevel(1);
 	}
-
-	public void SelectedExit()
-	{
+	public void SelectedExit(){
 		Application.Quit();
 	}
-
-	public void SelectedInstructions()
-	{
-
+	public void SelectedInstructions(){
+		MenuGUI.SetTrigger ("InstructionClicked");
 	}
-	public void SelectedLevel()
-	{
+	public void InstructionBackClicked(){
+		MenuGUI.SetTrigger ("IBackClicked");
+	}
+	public void SelectedLevel(){
 
 	}
 }
